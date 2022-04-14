aa76cae7ab5eb9dc71efd2d22b0a4681be1c4bb5
Deck Stream <tneswick2@verizon.net>
2022-04-13 20:21:22 -0700

    query for db

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2')
 const express = require('express')
 const PORT = process.argv.PORT || 3001
 const app = express();
@@ -5,6 +6,21 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+// Connect to Database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'election'
+    },
+    console.log('Connnected to the election database.')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
